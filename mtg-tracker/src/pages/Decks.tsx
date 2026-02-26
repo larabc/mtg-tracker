f9f1e4e968c90375778cab8d988055f38ee3b997
lara bautista <lara.bautista16@gmail.com>
2026-02-26 14:50:57 +0100

    import lucide icons for react

--- mtg-tracker/src/pages/Decks.tsx
+++ mtg-tracker/src/pages/Decks.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Drawer from "../components/Drawer";
 import DecksList from "../components/DecksList";
 import useDecks from "../hooks/useDecks";
+import { Plus } from 'lucide-react'
 
 export default function Decks() {
     const [isDrawerOpen, setDrawerOpen] = useState(false);
@@ -24,7 +25,7 @@ export default function Decks() {
                 <Drawer isOpen={isDrawerOpen} onClose={handleDrawerClose} createDeck={createDeck} />
             )}
             <DecksList decks={decks} />
-            <button className="fixed bottom-20 right-4 w-12 h-12 rounded-full bg-amber-400 text-zinc-950 text-2xl font-bold flex items-center justify-center shadow-lg" onClick={handleDeckCreation}>+</button>
+            <button className="fixed bottom-20 right-4 w-12 h-12 rounded-full bg-amber-400 text-zinc-950 text-2xl font-bold flex items-center justify-center shadow-lg" onClick={handleDeckCreation}><Plus className="w-5 h-5" /></button>
         </>
     );
 }
