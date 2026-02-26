692794f9caba0703d20d48da8c8073f302de6d23
lara bautista <lara.bautista16@gmail.com>
2026-02-26 12:38:01 +0100

    clean files and set basic tailwind classes on files

--- mtg-tracker/src/App.tsx
+++ mtg-tracker/src/App.tsx
@@ -10,14 +10,18 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/decks" />} />
-        <Route path="/decks" element={<Decks />} />
-        <Route path="/life-tracker" element={<LifeTracker />} />
-        <Route path="/new-match" element={<NewMatch />} />
-        <Route path="/stats" element={<Stats />} />
-      </Routes>
-      <Nav />
+      <div className="flex flex-col min-h-screen">
+        <main className="flex-1 overflow-y-auto pb-16">
+          <Routes>
+            <Route path="/" element={<Navigate to="/decks" />} />
+            <Route path="/decks" element={<Decks />} />
+            <Route path="/life-tracker" element={<LifeTracker />} />
+            <Route path="/new-match" element={<NewMatch />} />
+            <Route path="/stats" element={<Stats />} />
+          </Routes>
+        </main>
+        <Nav />
+      </div>
     </BrowserRouter>
   )
 }
--- mtg-tracker/src/components/nav.tsx
+++ mtg-tracker/src/components/nav.tsx
@@ -2,11 +2,10 @@ import { Link } from "react-router-dom";
 
 export default function Nav() {
     return (
-        <>
-            <Link to="/decks">Decks</Link>
+        <nav className="fixed bottom-0 w-full flex justify-around bg-zinc-900 py-3">            <Link to="/decks">Decks</Link>
             <Link to="/life-tracker">Life Tracker</Link>
             <Link to="/new-match">New Match</Link>
             <Link to="/stats">Stats</Link>
-        </>
+        </nav>
     )
 }
